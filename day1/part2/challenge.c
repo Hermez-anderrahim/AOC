948765b483ec56f7d95c0ca718e149d3f599881a
Hermez Abderahim <her.abderrahim.15@gmail.com>
2023-12-30 00:07:24 +0100

    day 1 part AOC

--- day1/part2/challenge.c
+++ day1/part2/challenge.c
@@ -117,48 +117,87 @@ int string_to_int(char StringNumber[])
     return -1;
 }
 
-void get_first_digit(char File_Line[], int *first_digit, int *i, int *j)
+void get_first_digit(char File_Line[], int *fin, int *j)
 {
 
-    int k;
+    int i = 0, k, firstDigit;
     char String_int[10];
 
-    while (*i < strlen(File_Line) && File_Line[*i] != '\n')
+    while (i < strlen(File_Line) && File_Line[i] != '\n')
     {
 
-        *j = *i;
+        j = i;
         k = 0;
-        while (*j < *i + 5)
+        while (j < i + 5)
         {
 
             if (isdigit(File_Line[*j]) == false)
             {
 
-                String_int[k] = File_Line[*j];
+                String_int[k] = File_Line[j];
 
                 printf("%s\n", String_int);
 
                 if (string_to_int(String_int) != -1)
                 {
 
-                    *first_digit = string_to_int(String_int);
-                    return;
+                    firstDigit = string_to_int(String_int);
+                    *fin = i;
                 }
-                *j++;
+                j++;
                 k++;
             }
             else
             {
-                *first_digit = File_Line[*j] - '0';
-                return;
+                return File_Line[j] - '0';
+                *fin = i;
             }
         }
-        *i++;
+        i++;
     }
+    *fin = i;
 }
 
-int get_last_digit(FILE *fp, char File_Line[])
+int get_last_digit(char File_Line[], int start_search)
 {
+
+    int last_digit;
+    int i = start_search, j, k;
+    char String_int[20];
+
+    while (i < strlen(File_Line) && File_Line[i] != '\n')
+    {
+        printf("im coucle 1");
+        j = i;
+        k = 0;
+        while (j < i + 5)
+        {
+            printf("im boucle 2");
+
+            if (isdigit(File_Line[j]) == false)
+            {
+
+                String_int[k] = File_Line[j];
+
+                printf("%s\n", String_int);
+
+                if (string_to_int(String_int) != -1)
+                {
+
+                    last_digit = string_to_int(String_int);
+                }
+            }
+            else
+            {
+                // he finds a numerical value
+                last_digit = File_Line[j] - '0';
+            }
+            j++;
+            k++;
+        }
+        i++;
+    }
+    return last_digit;
 }
 
 int main(int argc, char const *argv[])
@@ -166,10 +205,9 @@ int main(int argc, char const *argv[])
     FILE *fp = fopen("CalibrationDOC_2.txt", "r+");
 
     char File_Line[30];
-    int first_digit, last_digit, i = 0, j = 0;
+    int fin;
     fgets(File_Line, 40, fp);
-    get_first_digit(File_Line, &first_digit, &i, &j);
-    printf("the number is %d\n", first_digit);
+    printf("the last digit is %d", get_last_digit(File_Line, 1));
 
     return 0;
 }
